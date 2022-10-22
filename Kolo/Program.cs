d180d9acfdcc35ed5eb4619abbb9ca035ed85ef9
Marek <Marek@MAREK-LENOVOP53>
2022-10-22 20:35:31 +0200

    Dodaj pliki projektów.

---
+++ Kolo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double promień;
+            Console.WriteLine("Podaj promień :");
+            var linia = Console.ReadLine();
+
+            promień = Convert.ToDouble(linia);
+            var pole = Math.PI * promień * promień;
+            var obwód = Math.PI * 2.0 * promień;
+
+            Console.WriteLine("Pole koła o promieniu " + promień + " wynosi " + pole);
+            Console.WriteLine($"Obwód koła o promieniu {promień} wynosi {obwód}");
+        }
+    }
+}
